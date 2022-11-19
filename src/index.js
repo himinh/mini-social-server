e4c70eb1984f7170a564c94dc7b1fc9e0a8ecbf8
minh chiu <himinhchiu@gmail.com>
2022-11-19 08:41:13 +0700

    cors socker

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const server = app.listen(
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://symphonious-kheer-356f00.netlify.app',
   },
 })
 io.on('connection', socket => {
